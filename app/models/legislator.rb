e689c94bb5c71eb644552dd92372c075b6b1bf46
Apprentice <apprentice@devbootcamp.com>
2013-09-12 20:26:56 -0700

    Uploaded CSV file into ActiveRecord database

---
+++ app/models/legislator.rb
@@ -0,0 +1,13 @@
+require_relative '../../db/config'
+
+class Legislator < ActiveRecord::Base
+
+  def initialize
+      
+  end
+
+
+
+
+end
+
--- lib/sunlight_legislators_importer.rb
+++ lib/sunlight_legislators_importer.rb
@@ -1,17 +1,37 @@
 require 'csv'
+require_relative '../app/models/legislator'
+
+# wo_csv = Module.constants
+
+# w_csv = [:Object, :Module, :Class, :BasicObject, :Kernel, :NilClass, :NIL, :Data, :TrueClass, :TRUE, :FalseClass, :FALSE, :Encoding, :Comparable, :Enumerable, :String, :Symbol, :Exception, :SystemExit, :SignalException, :Interrupt, :StandardError, :TypeError, :ArgumentError, :IndexError, :KeyError, :RangeError, :ScriptError, :SyntaxError, :LoadError, :NotImplementedError, :NameError, :NoMethodError, :RuntimeError, :SecurityError, :NoMemoryError, :EncodingError, :SystemCallError, :Errno, :ZeroDivisionError, :FloatDomainError, :Numeric, :Integer, :Fixnum, :Float, :Bignum, :Array, :Hash, :ENV, :Struct, :RegexpError, :Regexp, :MatchData, :Marshal, :Range, :IOError, :EOFError, :IO, :STDIN, :STDOUT, :STDERR, :ARGF, :FileTest, :File, :Dir, :Time, :Random, :Signal, :Process, :Proc, :LocalJumpError, :SystemStackError, :Method, :UnboundMethod, :Binding, :Math, :GC, :ObjectSpace, :Enumerator, :StopIteration, :RubyVM, :Thread, :TOPLEVEL_BINDING, :ThreadGroup, :Mutex, :ThreadError, :Fiber, :FiberError, :Rational, :Complex, :RUBY_VERSION, :RUBY_RELEASE_DATE, :RUBY_PLATFORM, :RUBY_PATCHLEVEL, :RUBY_REVISION, :RUBY_DESCRIPTION, :RUBY_COPYRIGHT, :RUBY_ENGINE, :ARGV, :Gem, :RbConfig, :Config, :CROSS_COMPILING, :Forwardable, :SingleForwardable, :Date, :DateTime, :StringIO, :CSV]
+
+# puts "requiring CSV adds these: #{w_csv - wo_csv}"
+
 
 class SunlightLegislatorsImporter
   def self.import(filename)
     csv = CSV.new(File.open(filename), :headers => true)
     csv.each do |row|
+      politician = Legislator.new
       row.each do |field, value|
-        # TODO: begin
-        raise NotImplementedError, "TODO: figure out what to do with this row and do it!"
+         politician[field] = value
+        # p field.class
+
+        # p value.class
+        # p "*"*100
+        #attribute_hash = Hash[field, value]
+
+        # raise NotImplementedError, "TODO: figure out what to do with this row and do it!"
         # TODO: end
       end
+      politician.save
     end
   end
 end
+
+# p %x[pwd]
+SunlightLegislatorsImporter.import('db/data/legislators.csv')
+
 
 # IF YOU WANT TO HAVE THIS FILE RUN ON ITS OWN AND NOT BE IN THE RAKEFILE, UNCOMMENT THE BELOW
 # AND RUN THIS FILE FROM THE COMMAND LINE WITH THE PROPER ARGUMENT.
