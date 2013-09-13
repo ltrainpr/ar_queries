177b81febbdab19c8c025a19a6686887a976a7bd
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-12 21:05:41 -0700

    Repairing with Tyler

--- app/main.rb
+++ app/main.rb
@@ -1,1 +1,4 @@
 
+module Shiznitt
+  
+end
--- app/models/legislator.rb
+++ app/models/legislator.rb
@@ -1,11 +1,16 @@
 require_relative '../../db/config'
+require_relative '../main'
 
 class Legislator < ActiveRecord::Base
+  extend Shiznitt
 
   def initialize
       
   end
 
+  def self.by_state
+    
+  end
 
 
 
